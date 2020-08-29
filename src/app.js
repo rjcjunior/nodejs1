6d906600124de92c4fc0f533dc4a9e7b3fb459dd
Ricardo Junior <rjcjunior@hotmail.com>
2020-08-29 19:31:19 -0300

    Desafio finalizado

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid } = require('uuid');
+const { uuid } = require('uuidv4');
 
 const app = express();
 
@@ -11,23 +11,85 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories)
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const {title, url, techs, likes} = request.body;
+  let likeVariable = 0;
+
+  if (!likes && likes!=undefined){
+    likeVariable = likes; 
+  }
+
+  let repositorie = {id:uuid(), title, url, techs};
+
+  repositorie.likes = likeVariable;
+
+  repositories.push(repositorie);
+  return response.json(repositorie);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params;
+  const {title, url, techs, likes} = request.body;
+  const repositorieIndex = repositories.findIndex( repositorie => repositorie.id == id);
+  let likeVariable = 0;
+
+  if (repositorieIndex < 0){
+    return response.status(400).json({error:"Id not found"});
+  }
+
+  if (!likes && likes == undefined){
+    likeVariable = likes; 
+  }
+
+  let repositorie = { 
+    id,
+    title,
+    url,
+    techs,
+  }
+
+  repositorie.likes = likeVariable;
+
+  repositories[repositorieIndex] = repositorie;
+
+  return response.json(repositorie);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params;
+
+  const repositorieIndex = repositories.findIndex( repositorie => repositorie.id == id);
+
+  if (repositorieIndex < 0){
+    return response.status(400).json({error:"Id not found"});
+  }
+
+  repositories.splice(repositorieIndex, 1);
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const {id} = request.params;
+
+  const repositorieIndex = repositories.findIndex( repositorie => repositorie.id == id);
+
+  if (repositorieIndex < 0){
+    return response.status(400).json({error:"Id not found"});
+  }
+
+  if (repositories[repositorieIndex].likes && repositories[repositorieIndex].likes !=undefined){
+    repositories[repositorieIndex].likes += 1;
+  }
+
+  else{
+    repositories[repositorieIndex].likes = 1
+  }
+
+  return response.json(repositories[repositorieIndex]);
 });
 
 module.exports = app;
